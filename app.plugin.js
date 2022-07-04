f3e340629f876275d80b82d530a02f35f4690afa
Mankin <vinkencheng@gmail.com>
2022-07-04 13:00:43 +0800

    fix: 格式化代码时意外去掉的分号

--- app.plugin.js
+++ app.plugin.js
@@ -64,25 +64,25 @@ const setAppDelegate = config =>
         ) +
         `
   // JPush 初始化配置，可以延时初始化 不再强制在此初始化，在 js 里可以直接调用 init
-  // [JPUSHService setupWithOption:launchOptions appKey:@"${JPUSH_APPKEY}" channel:@"${JPUSH_CHANNEL}" apsForProduction:YES]
+  // [JPUSHService setupWithOption:launchOptions appKey:@"${JPUSH_APPKEY}" channel:@"${JPUSH_CHANNEL}" apsForProduction:YES];
 
   // APNS
-  JPUSHRegisterEntity * entity = [[JPUSHRegisterEntity alloc] init]
+  JPUSHRegisterEntity * entity = [[JPUSHRegisterEntity alloc] init];
   if (@available(iOS 12.0, *)) {
-    entity.types = JPAuthorizationOptionAlert|JPAuthorizationOptionBadge|JPAuthorizationOptionSound|JPAuthorizationOptionProvidesAppNotificationSettings
+    entity.types = JPAuthorizationOptionAlert|JPAuthorizationOptionBadge|JPAuthorizationOptionSound|JPAuthorizationOptionProvidesAppNotificationSettings;
   }
-  [JPUSHService registerForRemoteNotificationConfig:entity delegate:self]
-  [launchOptions objectForKey: UIApplicationLaunchOptionsRemoteNotificationKey]
+  [JPUSHService registerForRemoteNotificationConfig:entity delegate:self];
+  [launchOptions objectForKey: UIApplicationLaunchOptionsRemoteNotificationKey];
 
   // 自定义消息
-  NSNotificationCenter *defaultCenter = [NSNotificationCenter defaultCenter]
-  [defaultCenter addObserver:self selector:@selector(networkDidReceiveMessage:) name:kJPFNetworkDidReceiveMessageNotification object:nil]
+  NSNotificationCenter *defaultCenter = [NSNotificationCenter defaultCenter];
+  [defaultCenter addObserver:self selector:@selector(networkDidReceiveMessage:) name:kJPFNetworkDidReceiveMessageNotification object:nil];
 
   // 地理围栏
-  [JPUSHService registerLbsGeofenceDelegate:self withLaunchOptions:launchOptions]
+  [JPUSHService registerLbsGeofenceDelegate:self withLaunchOptions:launchOptions];
 
   #if defined(FB_SONARKIT_ENABLED) && __has_include(<FlipperKit/FlipperClient.h>)
-    InitializeFlipper(application)
+    InitializeFlipper(application);
   #endif
 ` +
         config.modResults.contents.slice(
@@ -97,16 +97,16 @@ const setAppDelegate = config =>
         `appKey:@"${JPUSH_APPKEY}" channel:@"${JPUSH_CHANNEL}" `
       )
     }
-    if (config.modResults.contents.indexOf('return [super application:application didRegisterForRemoteNotificationsWithDeviceToken:deviceToken]') > -1) {
-      config.modResults.contents = config.modResults.contents.replace('return [super application:application didRegisterForRemoteNotificationsWithDeviceToken:deviceToken]', '[JPUSHService registerDeviceToken:deviceToken]')
+    if (config.modResults.contents.indexOf('return [super application:application didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];') > -1) {
+      config.modResults.contents = config.modResults.contents.replace('return [super application:application didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];', '[JPUSHService registerDeviceToken:deviceToken];')
     }
-    if (config.modResults.contents.indexOf('return [super application:application didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler]') > -1) {
-      config.modResults.contents = config.modResults.contents.replace('return [super application:application didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler]', `
+    if (config.modResults.contents.indexOf('return [super application:application didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];') > -1) {
+      config.modResults.contents = config.modResults.contents.replace('return [super application:application didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];', `
       // iOS 10 以下 Required
-      NSLog(@"iOS 7 APNS")
-      [JPUSHService handleRemoteNotification:userInfo]
-      [[NSNotificationCenter defaultCenter] postNotificationName:J_APNS_NOTIFICATION_ARRIVED_EVENT object:userInfo]
-      completionHandler(UIBackgroundFetchResultNewData)
+      NSLog(@"iOS 7 APNS");
+      [JPUSHService handleRemoteNotification:userInfo];
+      [[NSNotificationCenter defaultCenter] postNotificationName:J_APNS_NOTIFICATION_ARRIVED_EVENT object:userInfo];
+      completionHandler(UIBackgroundFetchResultNewData);
       `)
     }
     if (
@@ -121,62 +121,62 @@ const setAppDelegate = config =>
 
         // //注册 APNS 成功并上报 DeviceToken
         // - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
-        //   [JPUSHService registerDeviceToken:deviceToken]
+        //   [JPUSHService registerDeviceToken:deviceToken];
         // }
 
         // //iOS 7 APNS
         // - (void)application:(UIApplication *)application didReceiveRemoteNotification:  (NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
         //   // iOS 10 以下 Required
-        //   NSLog(@"iOS 7 APNS")
-        //   [JPUSHService handleRemoteNotification:userInfo]
-        //   [[NSNotificationCenter defaultCenter] postNotificationName:J_APNS_NOTIFICATION_ARRIVED_EVENT object:userInfo]
-        //   completionHandler(UIBackgroundFetchResultNewData)
+        //   NSLog(@"iOS 7 APNS");
+        //   [JPUSHService handleRemoteNotification:userInfo];
+        //   [[NSNotificationCenter defaultCenter] postNotificationName:J_APNS_NOTIFICATION_ARRIVED_EVENT object:userInfo];
+        //   completionHandler(UIBackgroundFetchResultNewData);
         // }
 
         //iOS 10 前台收到消息
         - (void)jpushNotificationCenter:(UNUserNotificationCenter *)center  willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(NSInteger))completionHandler {
-          NSDictionary * userInfo = notification.request.content.userInfo
+          NSDictionary * userInfo = notification.request.content.userInfo;
           if([notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
             // Apns
-            NSLog(@"iOS 10 APNS 前台收到消息")
-            [JPUSHService handleRemoteNotification:userInfo]
-            [[NSNotificationCenter defaultCenter] postNotificationName:J_APNS_NOTIFICATION_ARRIVED_EVENT object:userInfo]
+            NSLog(@"iOS 10 APNS 前台收到消息");
+            [JPUSHService handleRemoteNotification:userInfo];
+            [[NSNotificationCenter defaultCenter] postNotificationName:J_APNS_NOTIFICATION_ARRIVED_EVENT object:userInfo];
           }
           else {
             // 本地通知 todo
-            NSLog(@"iOS 10 本地通知 前台收到消息")
-            [[NSNotificationCenter defaultCenter] postNotificationName:J_LOCAL_NOTIFICATION_ARRIVED_EVENT object:userInfo]
+            NSLog(@"iOS 10 本地通知 前台收到消息");
+            [[NSNotificationCenter defaultCenter] postNotificationName:J_LOCAL_NOTIFICATION_ARRIVED_EVENT object:userInfo];
           }
           //需要执行这个方法，选择是否提醒用户，有 Badge、Sound、Alert 三种类型可以选择设置
-          completionHandler(UNNotificationPresentationOptionAlert)
+          completionHandler(UNNotificationPresentationOptionAlert);
         }
 
         //iOS 10 消息事件回调
         - (void)jpushNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler: (void (^)(void))completionHandler {
-          NSDictionary * userInfo = response.notification.request.content.userInfo
+          NSDictionary * userInfo = response.notification.request.content.userInfo;
           if([response.notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
             // Apns
-            NSLog(@"iOS 10 APNS 消息事件回调")
-            [JPUSHService handleRemoteNotification:userInfo]
+            NSLog(@"iOS 10 APNS 消息事件回调");
+            [JPUSHService handleRemoteNotification:userInfo];
             // 保障应用被杀死状态下，用户点击推送消息，打开app后可以收到点击通知事件
-            [[RCTJPushEventQueue sharedInstance]._notificationQueue insertObject:userInfo atIndex:0]
-            [[NSNotificationCenter defaultCenter] postNotificationName:J_APNS_NOTIFICATION_OPENED_EVENT object:userInfo]
+            [[RCTJPushEventQueue sharedInstance]._notificationQueue insertObject:userInfo atIndex:0];
+            [[NSNotificationCenter defaultCenter] postNotificationName:J_APNS_NOTIFICATION_OPENED_EVENT object:userInfo];
           }
           else {
             // 本地通知
-            NSLog(@"iOS 10 本地通知 消息事件回调")
+            NSLog(@"iOS 10 本地通知 消息事件回调");
             // 保障应用被杀死状态下，用户点击推送消息，打开app后可以收到点击通知事件
-            [[RCTJPushEventQueue sharedInstance]._localNotificationQueue insertObject:userInfo atIndex:0]
-            [[NSNotificationCenter defaultCenter] postNotificationName:J_LOCAL_NOTIFICATION_OPENED_EVENT object:userInfo]
+            [[RCTJPushEventQueue sharedInstance]._localNotificationQueue insertObject:userInfo atIndex:0];
+            [[NSNotificationCenter defaultCenter] postNotificationName:J_LOCAL_NOTIFICATION_OPENED_EVENT object:userInfo];
           }
           // 系统要求执行这个方法
-          completionHandler()
+          completionHandler();
         }
 
         //自定义消息
         - (void)networkDidReceiveMessage:(NSNotification *)notification {
-          NSDictionary * userInfo = [notification userInfo]
-          [[NSNotificationCenter defaultCenter] postNotificationName:J_CUSTOM_NOTIFICATION_EVENT object:userInfo]
+          NSDictionary * userInfo = [notification userInfo];
+          [[NSNotificationCenter defaultCenter] postNotificationName:J_CUSTOM_NOTIFICATION_EVENT object:userInfo];
         }
 
         //************************************************JPush end************************************************
@@ -340,7 +340,7 @@ const setSettingsGradle = config =>
         config.modResults.contents +
         `
 include ':jpush-react-native'
-project(':jpush-react-native').projectDir = new File(["node", "--print", "require.resolve('jpush-react-native/package.json')"].execute(null, rootDir).text.trim(), "../android");`
+project(':jpush-react-native').projectDir = new File(["node", "--print", "require.resolve('jpush-react-native/package.json')"].execute(null, rootDir).text.trim(), "../android")`
     }
     if (
       config.modResults.contents.indexOf(`include ':jcore-react-native'`) === -1
@@ -350,7 +350,7 @@ project(':jpush-react-native').projectDir = new File(["node", "--print", "requir
         config.modResults.contents +
         `
 include ':jcore-react-native'
-project(':jcore-react-native').projectDir = new File(["node", "--print", "require.resolve('jcore-react-native/package.json')"].execute(null, rootDir).text.trim(), "../android");`
+project(':jcore-react-native').projectDir = new File(["node", "--print", "require.resolve('jcore-react-native/package.json')"].execute(null, rootDir).text.trim(), "../android")`
     }
 
     return config
